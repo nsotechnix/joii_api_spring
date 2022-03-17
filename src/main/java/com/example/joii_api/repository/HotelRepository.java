7ac75b55d669a5ef5f5c0ac632850d15598eab48
nsotechnix <ns.otechnix@gmail.com>
2022-03-17 10:24:19 +0530

    fixed status issue of hotels

--- src/main/java/com/example/joii_api/repository/HotelRepository.java
+++ src/main/java/com/example/joii_api/repository/HotelRepository.java
@@ -11,11 +11,12 @@ import org.springframework.stereotype.Repository;
 public interface HotelRepository extends JpaRepository<Hotels, String> {
     @Query(value = "SELECT * FROM hotels WHERE status = 1", nativeQuery = true)
     List <Hotels>findAvailableHotels();
+    @Query(value = "SELECT * FROM hotels WHERE city = ?1 AND status= 1", nativeQuery = true)
     List<Hotels>findAllByCity(String  city);
     Optional<Hotels>findById(Long id);
    List <Hotels>findAllByOrderByPriceAsc();
    List <Hotels>findAllByOrderByPriceDesc();
-    @Query(value = "SELECT * FROM hotels WHERE property_type = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM hotels WHERE property_type = ?1 AND status=1", nativeQuery = true)
    List <Hotels>findHotelsByProperty_type(String property_type);
 
 }
